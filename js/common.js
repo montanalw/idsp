0f7ac847517972594124185af47761f2b99ec34c
younsoolim <ylim27@my.bcit.ca>
2021-05-04 22:02:46 -0700

    added header js function - scroll down

--- js/common.js
+++ js/common.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
   backToTop()
+  scrollNav()
 })
 
+// BACK TO TOP FUNCTION
 function backToTop(){
   $(".backToTop").click(function(){
     $("html, body").animate({scrollTop: 0}, "slow");
@@ -9,6 +11,7 @@ function backToTop(){
   })
 }
 
+// VIDEO MOUSEENTER & MOUSELEAVE
 const VideoController = function(isHovering, videoElement)
 {
   if(isHovering == false)
@@ -19,5 +22,44 @@ const VideoController = function(isHovering, videoElement)
   {
     videoElement.pause();
   }
+}
+
+// NAVIGATION SCROLL DOWN
+function scrollNav(){
+  // Hide Header on on scroll down
+  var didScroll;
+  var lastScrollTop = 0;
+  var delta = 5;
+  var navbarHeight = $('header').outerHeight();
+
+  $(window).scroll(function(event){
+      didScroll = true;
+  });
+          
+  setInterval(function() {
+      if (didScroll) {
+          hasScrolled();
+          didScroll = false;
+      }
+  }, 250);
+
+  function hasScrolled() {
+      var st = $(this).scrollTop();
+      
+      // Make sure they scroll more than delta
+      if(Math.abs(lastScrollTop - st) <= delta)
+          return;
+      
+      if (st > lastScrollTop && st > navbarHeight){
+          // Scroll Down
+          $('header').removeClass('nav-down').addClass('nav-up');
+      } else {
+          // Scroll Up
+          if(st + $(window).height() < $(document).height()) {
+              $('header').removeClass('nav-up').addClass('nav-down');
+          }
+      }      
+      lastScrollTop = st;
+  } 
 }
 
